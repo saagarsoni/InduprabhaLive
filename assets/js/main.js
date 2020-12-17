79b0268da52c16d460e2295098335d892847a57c
saagarsoni <saagarsoni3@gmail.com>
2020-12-17 09:20:55 +0530

    test added

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,3 +219,10 @@
 $(document).ready(function() {
   $('#rateMe4').mdbRate();
 });
+// Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
